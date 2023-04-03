90b0b911e8c93e5fb6dc337cd587ea4b5849a436
Arkadiusz <arecki358@gmail.com>
2023-04-03 22:38:20 +0200

    added method to escape polish characters (not working?), removed unnecessary image replacing class

--- src/main/java/com/arje/pdf/PdfWriter.java
+++ src/main/java/com/arje/pdf/PdfWriter.java
@@ -16,6 +16,37 @@ public class PdfWriter {
 
     private static final String PDF = ".pdf";
 
+    private String escapeHTML(String s){
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'ą': sb.append("&#261;"); break;
+                case 'ć': sb.append("&#263;"); break;
+                case 'ę': sb.append("&#281;"); break;
+                case 'ł': sb.append("&#322;"); break;
+                case 'ń': sb.append("&#324;"); break;
+                case 'ó': sb.append("&#243;"); break;
+                case 'ś': sb.append("&#347;"); break;
+                case 'ź': sb.append("&#378;"); break;
+                case 'ż': sb.append("&#380;"); break;
+                case 'Ą': sb.append("&#260;"); break;
+                case 'Ć': sb.append("&#262;"); break;
+                case 'Ę': sb.append("&#280;"); break;
+                case 'Ł': sb.append("&#321;"); break;
+                case 'Ń': sb.append("&#323;"); break;
+                case 'Ó': sb.append("&#211;"); break;
+                case 'Ś': sb.append("&#346;"); break;
+                case 'Ź': sb.append("&#377;"); break;
+                case 'Ż': sb.append("&#379;"); break;
+
+                default: sb.append(c); break;
+            }
+        }
+        return sb.toString();
+    }
+
     public void writePDF(String pathToSourceHtml) {
         File inputHTML = new File(pathToSourceHtml);
 
@@ -33,7 +64,8 @@ public class PdfWriter {
             sharedContext.setMedia("screen");
 //            sharedContext.setPrint(true);
             sharedContext.setInteractive(false);
-            sharedContext.setReplacedElementFactory(new MediaReplacedElementFactory(renderer.getSharedContext().getReplacedElementFactory()));
+//            sharedContext.setReplacedElementFactory(new MediaReplacedElementFactory(renderer.getSharedContext().getReplacedElementFactory()));
+//            renderer.setDocumentFromString(escapeHTML(document.html()));
             renderer.setDocumentFromString(document.html());
             renderer.layout();
             renderer.createPDF(outputStream);
